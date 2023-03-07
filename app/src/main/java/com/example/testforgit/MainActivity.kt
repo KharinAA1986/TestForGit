b70a39cd66cf50b0a2b4f6c36bd5afb9a8bfa130
KharinAA1986 <kharinaa1986@gmail.com>
2023-03-07 21:07:12 +0500

    Added EditText Added OnClickListener for button (print text from EditText to log)

--- app/src/main/java/com/example/testforgit/MainActivity.kt
+++ app/src/main/java/com/example/testforgit/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.testforgit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+
+
 
 class MainActivity : AppCompatActivity() {
+    private val tag: String = "TAG"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btn: Button = findViewById(R.id.button)
+        val eT: EditText = findViewById(R.id.editText)
+
+        btn.setOnClickListener {
+            Log.d(tag,eT.text.toString())
+        }
     }
 }
