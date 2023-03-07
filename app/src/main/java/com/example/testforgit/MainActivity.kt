5ba98d97ad8eee4408862f9de148471ea95de19a
KharinAA1986 <kharinaa1986@gmail.com>
2023-03-07 22:16:51 +0500

    Added TextView, CheckBox, ProgressBar. Changed onClickListener. Tested. It works

--- app/src/main/java/com/example/testforgit/MainActivity.kt
+++ app/src/main/java/com/example/testforgit/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.testforgit
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.TextView
 import timber.log.Timber
 
 
@@ -13,9 +16,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val btn: Button = findViewById(R.id.button)
         val eT: EditText = findViewById(R.id.editText)
+        val cb: CheckBox = findViewById(R.id.checkBox)
+        val tw: TextView = findViewById(R.id.textView)
+        val pb:ProgressBar = findViewById(R.id.progressBar)
+
 
         btn.setOnClickListener {
             Timber.d(eT.text.toString())
+            if(cb.isChecked) {
+                pb.progress = pb.progress + 10
+                tw.text = eT.text
+            }
         }
     }
 }
